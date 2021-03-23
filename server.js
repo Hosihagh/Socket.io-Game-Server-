112476c2429788358b287921f42de29ee710b39b
Hosein <h.haghayeghi@gmail.com>
2021-03-23 14:11:30 +0430

    Implemented disconnecting and spawning

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server started');
 
-io.on('connection', (socket) => {
-    console.log('client connected');
+let playerCount = 0;
 
+io.on('connection', (socket) => {
+    console.log('client connected, spawning');
+    socket.broadcast.emit('spawn');
+    playerCount++;
+
+    for (i = 0; i < playerCount; i++) {
+        socket.emit('spawn');
+        console.log('spending spawn to new player');
+    }
 
     socket.on('move', (data) => {
         console.log('client moved');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+        playerCount--;
     })
 })
